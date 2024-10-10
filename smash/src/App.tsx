b0c0f4491d92cbbfa708afbdab949c1443aaa2d3
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2024-10-10 11:29:57 -0300

    criacao do componente banner

--- smash/src/App.tsx
+++ smash/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-     
+      <Banner/>
     </div>
   );
 }
---
+++ smash/src/components/Banner/index.tsx
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <header className='banner'>
+            <img src="./imagens/banner1.jpg" alt="foto do banner" />
+        </header>
+    );
+}
+
+export default Banner;
